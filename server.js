1fc58796d41abc083508126b1e3b2963ccfabf23
Narendra <narendraraghuwanshi8192@gmail.com>
2026-05-05 06:07:56 +0530

    Bind to 0.0.0.0

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/team-task-
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
