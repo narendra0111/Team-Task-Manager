6f714752a06deac46a4701306def978a9f01f41e
Narendra <narendraraghuwanshi8192@gmail.com>
2026-05-05 05:44:53 +0530

    Log db errors

--- server.js
+++ server.js
@@ -34,4 +34,7 @@ mongoose.connect(MONGO_URI)
       console.log(`Server running on port ${PORT}`);
     });
   })
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  });
